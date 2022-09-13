451f7981e70d66a541fb3974833cafb13d81554f
fsampson30 <fsampson30@gmail.com>
2022-09-13 14:47:15 -0300

    Bug corrections.

--- app/src/main/java/com/sampson/algorithmtest/utils/SmallerInt.kt
+++ app/src/main/java/com/sampson/algorithmtest/utils/SmallerInt.kt
@@ -3,13 +3,14 @@ package com.sampson.algorithmtest.utils
 class SmallerInt {
 
     fun testSmallerInt(): Int {
-        val ints = arrayListOf(66, 12, 13, 14, 2)
+        val ints = arrayListOf(-66, -12, -13, -14, -17, 202, 900, 99999, -1000,5,-1)
         var smallerNumber = 0
 
         for (i in 0..ints.lastIndex) {
             if (i == 0) {
                 smallerNumber = ints[i]
-            }else if (ints[i] < smallerNumber) {
+            }
+            if (Math.abs(ints[i]) < Math.abs(smallerNumber)) {
                 smallerNumber = ints[i]
             }
         }
